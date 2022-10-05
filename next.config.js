d13027b1934c551db7b3c8e901c7c66bd7068a25
Juan Gómez <gomezj01@stepstone.com>
2022-10-05 15:16:04 -0600

    pwa con next

--- next.config.js
+++ next.config.js
@@ -28,4 +28,10 @@ const nextConfig = {
   // }
 }
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+});
+
+module.exports = withPWA(nextConfig);
